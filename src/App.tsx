61d4aa9516ccbb3e6e640bc357b91d98f927e4d2
the-sociophobic <torikowashi@gmail.com>
2025-06-18 20:56:42 +0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import Text from './components/Text'
 
 import './assets/styles/index.sass'
-import Button from './components/Button'
 import Input from './components/Input'
 
 
